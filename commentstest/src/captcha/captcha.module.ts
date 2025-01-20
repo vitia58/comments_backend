b33fa9c9b99b094b81339d2ba3052467ceb7e16e
Vitia <vitiacraft@gmail.com>
2025-01-20 15:00:36 +0200

    feat(Cache): implemented

--- commentstest/src/captcha/captcha.module.ts
+++ commentstest/src/captcha/captcha.module.ts
@@ -1,29 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CaptchaController } from './captcha.controller';
 import { CaptchaService } from './captcha.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        imports: [ConfigModule],
-        useFactory: async (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URI')],
-            queue: configService.get<string>('RABBITMQ_QUEUE'),
-            queueOptions: {
-              durable: false,
-            },
-          },
-        }),
-        inject: [ConfigService],
-        name: 'CAPTCHA_SERVICE',
-      },
-    ]),
-  ],
   controllers: [CaptchaController],
   providers: [CaptchaService],
   exports: [CaptchaService],
--- commentstest/src/topics/topics.service.ts
+++ commentstest/src/topics/topics.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Comment, CommentDocument } from 'src/models/comments.model';
 import { TopicPipelines } from './topics.pipelines';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class TopicsService {
   constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
     @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
   ) {}
 
   async list() {
+    console.log('Fetching topics list');
     return this.commentModel.aggregate<
       Pick<Comment, '_id' | 'name' | 'email' | 'text' | 'date'>
     >(TopicPipelines.getCommentHeadPipeline());
+  }
+
+  @OnEvent('topic.created')
+  updateCache() {
+    this.cacheManager.del('topicsList');
   }
 }
